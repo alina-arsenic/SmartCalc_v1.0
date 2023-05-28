a8561518831aa555fc1830fb86e4e484c66a934c
weaveryu <weaveryu@student.21-school.ru>
2023-05-28 14:34:41 +0300

    structure change and credit gui

--- src/gui_main.c
+++ src/gui_main.c
@@ -2,32 +2,11 @@
 #include "gui_utils.h"
 #include "lib/s21_notation.h"
 
-GtkDrawingArea *drawingArea;
-cairo_t *cr;
-GtkWidget *label;
-GtkWidget *image;
-GtkWidget *box;
 s21_limits limits;
 s21_numpad numpad;
-char output_buffer[MAX_LEN] = {0};
-char input_buffer[MAX_LEN] = {0};
-char expr[MAX_LEN] = {0};
+s21_credit credit;
 
-int check_x_count(char *expr) {
-	int count = 0;
-	char *ptr = expr;
-	while (ptr = strstr(ptr, "x")) {
-        count++;
-        ptr++;
-    }
-	if (count > 15) {
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-int parse_number(char *str, double *result) {
+int parse_double(char *str, double *result) {
 	int i = 0, dot = 0;
 	if (str[i] == '-') i++;
 	while (is_digit(str[i])) i++;
@@ -48,159 +27,99 @@ int parse_number(char *str, double *result) {
 	return 0;
 }
 
-int check_limits(s21_limits *lims) {
-	memset(lims->x1_buffer, 0, MAX_LEN);
-	memset(lims->x2_buffer, 0, MAX_LEN);
-	memset(lims->y1_buffer, 0, MAX_LEN);
-	memset(lims->y2_buffer, 0, MAX_LEN);
-	strcat(lims->x1_buffer, gtk_entry_get_text(GTK_ENTRY(lims->x1_box)));
-	strcat(lims->x2_buffer, gtk_entry_get_text(GTK_ENTRY(lims->x2_box)));
-	strcat(lims->y1_buffer, gtk_entry_get_text(GTK_ENTRY(lims->y1_box)));
-	strcat(lims->y2_buffer, gtk_entry_get_text(GTK_ENTRY(lims->y2_box)));
-	if (!strcmp("", lims->x1_buffer) || !strcmp("", lims->x2_buffer) ||
-		!strcmp("", lims->y1_buffer) || !strcmp("", lims->y2_buffer)) {
+int parse_integer(char *str, int *result) {
+	int i = 0;
+	if (str[i] == '-') i++;
+	while (is_digit(str[i])) i++;
+	if (i != strlen(str)) {
 		return 1;
 	}
-	if (parse_number(lims->x1_buffer, &lims->x1) || parse_number(lims->x2_buffer, &lims->x2) ||
-		parse_number(lims->y1_buffer, &lims->y1) || parse_number(lims->y2_buffer, &lims->y2)) {
+    sscanf(str, "%d", result);
+	return 0;
+}
+
+void check_box_1(GtkButton *button, gpointer data) {
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[2]))) {
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(credit.button[3]), FALSE);
+	}
+	
+}
+
+void check_box_2(GtkButton *button, gpointer data) {
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[3]))) {
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(credit.button[2]), FALSE);
+	}
+}
+
+int check_input() {
+	char buf[MAX_LEN] = {0};
+	strcat(buf, gtk_entry_get_text(GTK_ENTRY(credit.entry_amount)));
+	if (buf[0] == '\0') {
+		return 1;
+	} else if (parse_double(buf, &credit.value_amount)) {
+		return 2;
+	} else if ((credit.value_amount <= 0.0) || (credit.value_amount > 1000000000000.0)) {
 		return 3;
 	}
-	if (fabs(lims->x1) > 1000000.0 || fabs(lims->x2) > 1000000.0 ||
-		fabs(lims->y1) > 1000000.0 || fabs(lims->y2) > 1000000.0) {
+	memset(buf, 0, MAX_LEN);
+	strcat(buf, gtk_entry_get_text(GTK_ENTRY(credit.entry_term)));
+	if (buf[0] == '\0') {
+		return 1;
+	} else if (parse_integer(buf, &credit.value_term)) {
 		return 2;
-	}
-	if (lims->x1 == lims->x2 || lims->y1 == lims->y2) {
+	} else if ((credit.value_term < 1) || (credit.value_term > 600)) {
 		return 4;
 	}
-	double temp;
-	if (lims->x1 > lims->x2) {
-		temp = lims->x1;
-		lims->x1 = lims->x2;
-		lims->x2 = temp;
+	memset(buf, 0, MAX_LEN);
+	strcat(buf, gtk_entry_get_text(GTK_ENTRY(credit.entry_rate)));
+	if (buf[0] == '\0') {
+		return 1;
+	} else if (parse_double(buf, &credit.value_rate)) {
+		return 2;
+	} else if ((credit.value_rate <= 0.0) || (credit.value_rate > 999.0)) {
+		return 5;
 	}
-	if (lims->y1 > lims->y2) {
-		temp = lims->y1;
-		lims->y1 = lims->y2;
-		lims->y2 = temp;
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[2])) == FALSE
+	 && gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[3])) == FALSE) {
+		return 6;
+	}
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[2]))) {
+		credit.credit_type = 1;
+	}
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(credit.button[3]))) {
+		credit.credit_type = 2;
 	}
 	return 0;
 }
 
-int make_plot(char *expr) {
-	GdkWindow *window = gtk_widget_get_window((GtkWidget*)drawingArea);
-	cairo_region_t *cairoRegion = cairo_region_create();
-	GdkDrawingContext *drawingContext = gdk_window_begin_draw_frame (window, cairoRegion);
-	cr = gdk_drawing_context_get_cairo_context (drawingContext);
-
-	fill_area(cr);
-	draw_border(cr);
-
-	if (expr[0]) {
-		double min = 0, max = 0;
-		draw_axis(cr, limits);
-		draw_plot(cr, limits, expr, &min, &max);
-
-		if (limits.y1 > max || limits.y2 < min) {
-			char buf[MAX_LEN] = {0};
-			sprintf(buf, "Graph is not visible, y ∈ [ %g ; %g ]", min, max);
-			gtk_label_set_text(GTK_LABEL(label), buf);
-		}
-	}
-
-	gdk_window_end_draw_frame(window, drawingContext);
-	cairo_region_destroy(cairoRegion);
-
-	return 0;
-}
-
-void calculate(GtkButton *button, gpointer data) {
-	
-	double result = 0;
-	const gchar* text = gtk_button_get_label(button);
-	gtk_label_set_text(GTK_LABEL(label), "");
-
-	memset(expr, 0, MAX_LEN);
-	//make_plot(expr);
-
-	if (strcmp("=", text) == 0) {
-
-		memset(input_buffer, 0, MAX_LEN);
-		memset(output_buffer, 0, MAX_LEN);
-		//memset(expr, 0, MAX_LEN);
-		//make_plot(expr);
-		strcat(input_buffer, gtk_entry_get_text(GTK_ENTRY(box)));
-		//gtk_entry_set_text(GTK_ENTRY(box), "");
-
-		int code = notation_convert(input_buffer, expr);
-		if (!code) {
-			if (strchr(input_buffer, 'x')) {
-				code += check_limits(&limits);
-				if (code == 0) {
-					code += check_x_count(expr);
-					if (code == 0) {
-						char buf[MAX_LEN] = {0};
-						double x_scale = fabs(limits.x1 - limits.x2) / (double)(DISPLAY_WIDTH);
-						double y_scale = fabs(limits.y1 - limits.y2) / (double)(DISPLAY_HEIGHT);
-						sprintf(buf, "X   1 : %g      Y   1 : %g", x_scale, y_scale);
-						gtk_label_set_text(GTK_LABEL(label), buf);
-						make_plot(expr);
-					} else {
-						gtk_label_set_text(GTK_LABEL(label), "No more than 15 'x' please");
-					}
-					
-				} else {
-					if (code == 1) {
-						gtk_label_set_text(GTK_LABEL(label), "Please specify limits");
-					} else if (code == 2) {
-						gtk_label_set_text(GTK_LABEL(label), "Values in limits must be from -1000000 to 1000000");
-					} else if (code == 4) {
-						gtk_label_set_text(GTK_LABEL(label), "Values in limits must be different");
-					} else {
-						gtk_label_set_text(GTK_LABEL(label), "Incorrect symbols in limits");
-					}
-				}
-			} else {
-				code += calculation(expr, &result);
-				memset(expr, 0, MAX_LEN);
-				if (!code) {
-					sprintf(output_buffer, "%.7lf", result);
-					char *dot_point = strchr(output_buffer, ',');
-					if (dot_point) *dot_point = '.';
-					gtk_entry_set_text(GTK_ENTRY(box), output_buffer);
-				} else {
-					gtk_label_set_text(GTK_LABEL(label), "Answer is not defined");
-				}
-			}
-		} else {
-			gtk_label_set_text(GTK_LABEL(label), "Incorrect expression");
-		}
-
-	} else if (strcmp("C", text) == 0) {
-		//memset(expr, 0, MAX_LEN);
-		//make_plot(expr);
-		gtk_entry_set_text(GTK_ENTRY(box), "");
-
-	} else if (strcmp("clear X", text) == 0) {
-		gtk_entry_set_text(GTK_ENTRY(limits.x1_box), "");
-		gtk_entry_set_text(GTK_ENTRY(limits.x2_box), "");
-
-	} else if (strcmp("clear Y", text) == 0) {
-		gtk_entry_set_text(GTK_ENTRY(limits.y1_box), "");
-		gtk_entry_set_text(GTK_ENTRY(limits.y2_box), "");
-
-	} else {
-		GtkWidget *box_ptr = gtk_window_get_focus(GTK_WINDOW(numpad.window));
-		guint position = gtk_editable_get_position(GTK_EDITABLE(box_ptr));
-		gtk_entry_buffer_insert_text(gtk_entry_get_buffer(GTK_ENTRY(box_ptr)), position, text, -1);
-		gtk_editable_set_position(GTK_EDITABLE(box_ptr), position + strlen(text));
-
+void credit_calculate(GtkButton *button, gpointer data) {
+	int code = check_input();
+	if (code == 1) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Please fill all entries");
+	} else if (code == 2) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Incorrect symbols in values");
+	} else if (code == 3) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Credit amount must be >0 and <=1000000000000 RUB");
+	} else if (code == 4) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Term must be >=1 and <=600 mounth");
+	} else if (code == 5) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Interest rate must be >0 and <=999 \%");
+	} else if (code == 6) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "Please choose type of monthly payments");
+	} else if (code == 0) {
+		gtk_label_set_text(GTK_LABEL(credit.message), "");
+		//
 	}
 }
 
-gboolean on_draw (GtkWidget *widget, GdkEventExpose *event, gpointer data) {
-	make_plot(expr);
-	//if (cr) cairo_restore(cr);
-	return FALSE;
+void credit_reset(GtkButton *button, gpointer data) {
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(credit.button[2]), FALSE);
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(credit.button[3]), FALSE);
+	credit.credit_type = 0;
+	gtk_label_set_text(GTK_LABEL(credit.message), "");
+	gtk_entry_set_text(GTK_ENTRY(credit.entry_amount), "");
+	gtk_entry_set_text(GTK_ENTRY(credit.entry_rate), "");
+	gtk_entry_set_text(GTK_ENTRY(credit.entry_term), "");
 }
 
 void activate(GtkApplication *app, gpointer user_data) {
@@ -218,13 +137,63 @@ void activate(GtkApplication *app, gpointer user_data) {
 	numpad.notebook = gtk_notebook_new();
 	gtk_notebook_set_show_border(GTK_NOTEBOOK(numpad.notebook), FALSE);
 	numpad.grid = gtk_grid_new();
-	gtk_notebook_append_page(GTK_NOTEBOOK(numpad.notebook), numpad.grid, NULL);
+	GtkWidget *notebook_label = gtk_label_new("Math");
+	gtk_notebook_append_page(GTK_NOTEBOOK(numpad.notebook), numpad.grid, notebook_label);
 	gtk_container_add(GTK_CONTAINER(numpad.window), numpad.notebook);
 
+	credit.grid = gtk_grid_new();
+	notebook_label = gtk_label_new("Credit");
+	gtk_notebook_append_page(GTK_NOTEBOOK(numpad.notebook), credit.grid, notebook_label);
+	credit.entry_amount = gtk_entry_new();
+	credit.entry_rate = gtk_entry_new();
+	credit.entry_term = gtk_entry_new();
+	credit.label[0] = gtk_label_new("Credit amount");
+	credit.label[1] = gtk_label_new("Credit term");
+	credit.label[2] = gtk_label_new("Interest rate");
+	credit.label[3] = gtk_label_new("RUB");
+	credit.label[4] = gtk_label_new("Months");
+	credit.label[5] = gtk_label_new("\%");
+	credit.label[6] = gtk_label_new("Type of monthly payments");
+	credit.message = gtk_label_new("Message");
+	for (int i = 0; i < 7; i++) {
+		gtk_label_set_xalign(GTK_LABEL(credit.label[i]), 0.0);
+	}
+	gtk_label_set_xalign(GTK_LABEL(credit.message), 0.0);
+	credit.text_wall = gtk_text_view_new();
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(credit.text_wall), FALSE);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[0], 0, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[1], 0, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[2], 0, 2, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.entry_amount, 1, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.entry_term, 1, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.entry_rate, 1, 2, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[3], 2, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[4], 2, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[5], 2, 2, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.label[6], 0, 3, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.message, 0, 6, 4, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.text_wall, 0, 7, 1, 1);
+
+	credit.button[0] = gtk_button_new_with_label("Calculate");
+	credit.button[1] = gtk_button_new_with_label("Reset");
+	credit.button[2] = gtk_check_button_new_with_label("annuity");
+	credit.button[3] = gtk_check_button_new_with_label("differentiated");
+	credit.credit_type = 0;
+	g_signal_connect(credit.button[0], "clicked", G_CALLBACK(credit_calculate), NULL);
+	g_signal_connect(credit.button[1], "clicked", G_CALLBACK(credit_reset), NULL);
+	g_signal_connect(credit.button[2], "clicked", G_CALLBACK(check_box_1), NULL);
+	g_signal_connect(credit.button[3], "clicked", G_CALLBACK(check_box_2), NULL);
+
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.button[0], 1, 5, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.button[1], 0, 5, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.button[2], 1, 3, 1, 1);
+	gtk_grid_attach(GTK_GRID(credit.grid), credit.button[3], 1, 4, 1, 1);
+	
+
 	// создание окна ввода выражения и вывода результата
-  	box = gtk_entry_new();
-	gtk_entry_set_max_length(GTK_ENTRY(box), MAX_USER_LEN);
-  	gtk_grid_attach(GTK_GRID(numpad.grid), box, 0, 0, 7, 1);
+  	numpad.box = gtk_entry_new();
+	gtk_entry_set_max_length(GTK_ENTRY(numpad.box), MAX_USER_LEN);
+  	gtk_grid_attach(GTK_GRID(numpad.grid), numpad.box, 0, 0, 7, 1);
 
 	// создание кнопок
 	initialize_buttons(&numpad);
@@ -233,11 +202,12 @@ void activate(GtkApplication *app, gpointer user_data) {
 	}
   	
 	// создание области для отрисовки графиков
-	drawingArea = (GtkDrawingArea*) gtk_drawing_area_new();
-	gtk_widget_set_size_request ((GtkWidget*)drawingArea, DISPLAY_WIDTH, DISPLAY_HEIGHT);
-    g_signal_connect((GtkWidget*)drawingArea, "draw", G_CALLBACK(on_draw), NULL);
+	numpad.drawingArea = (GtkDrawingArea*) gtk_drawing_area_new();
+	gtk_widget_set_size_request ((GtkWidget*)numpad.drawingArea, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	memset(numpad.expr, 0, MAX_LEN);
+    g_signal_connect((GtkWidget*)numpad.drawingArea, "draw", G_CALLBACK(on_draw), NULL);
 	gtk_grid_insert_row(GTK_GRID(numpad.grid), 0);
-	gtk_grid_attach(GTK_GRID(numpad.grid), (GtkWidget*)drawingArea, 0, 0, 7, 1);
+	gtk_grid_attach(GTK_GRID(numpad.grid), (GtkWidget*)numpad.drawingArea, 0, 0, 7, 1);
 
 	// создание области с областью определения и областью значений
 	limits.x1_box = gtk_entry_new();
@@ -252,12 +222,12 @@ void activate(GtkApplication *app, gpointer user_data) {
 	// создание области с текстовыми сообщениями
 	space = gtk_label_new("");
 	gtk_grid_attach(GTK_GRID(upper_grid), space, 0, 2, 7, 1);
-	label = gtk_label_new("");
-	gtk_label_set_xalign(GTK_LABEL(label), 0.0);
-	gtk_grid_attach(GTK_GRID(upper_grid), label, 0, 3, 7, 1);
+	numpad.label = gtk_label_new("");
+	gtk_label_set_xalign(GTK_LABEL(numpad.label), 0.0);
+	gtk_grid_attach(GTK_GRID(upper_grid), numpad.label, 0, 3, 7, 1);
 
 	// создание картинки
-	image = gtk_image_new_from_file("images/kitty2.png");
+	GtkWidget *image = gtk_image_new_from_file("images/kitty2.png");
     gtk_grid_attach(GTK_GRID(upper_grid), image, 7, 0, 1, 4);
 
 	// скрепление областей
@@ -270,7 +240,7 @@ void activate(GtkApplication *app, gpointer user_data) {
 	space = gtk_label_new("");
 	gtk_grid_attach(GTK_GRID(numpad.grid), space, 0, 1, 1, 1);
 
-	gtk_label_set_text(GTK_LABEL(label), "Welcome to the Smart Calc by weaveryu!");
+	gtk_label_set_text(GTK_LABEL(numpad.label), "Welcome to the Smart Calc by weaveryu!");
 	gtk_widget_show_all(numpad.window);
 
 }
--- src/gui_utils.h
+++ src/gui_utils.h
@@ -12,13 +12,6 @@
 #define APP_HEIGHT 600
 
 typedef struct {
-  GtkWidget *window;
-  GtkWidget *notebook;
-  GtkWidget *grid;
-  GtkWidget *button[33];
-} s21_numpad;
-
-typedef struct {
     GtkWidget *x1_box;
     GtkWidget *x2_box;
     GtkWidget *y1_box;
@@ -29,6 +22,42 @@ typedef struct {
     char y2_buffer[MAX_LEN];
     double x1, x2, y1, y2;
 } s21_limits;
+
+typedef struct {
+  GtkWidget *window;
+  GtkWidget *notebook;
+  GtkWidget *grid;
+  GtkWidget *label;
+  GtkWidget *button[33];
+  GtkWidget *box;
+  GtkDrawingArea *drawingArea;
+  cairo_t *cr;
+  char expr[MAX_LEN];
+} s21_numpad;
+
+typedef struct {
+  GtkWidget *grid;
+  GtkWidget *button[4];
+  GtkWidget *entry_amount;
+  GtkWidget *entry_term;
+  GtkWidget *entry_rate;
+  double value_amount;
+  int value_term;
+  double value_rate;
+  GtkWidget *label[15];
+  GtkWidget *message;
+  GtkWidget *text_wall;
+  int credit_type;
+} s21_credit;
+
+extern s21_limits limits;
+extern s21_numpad numpad;
+extern s21_credit credit;
+
+void calculate(GtkButton *button, gpointer data);
+gboolean on_draw (GtkWidget *widget, GdkEventExpose *event, gpointer data);
+int parse_double(char *str, double *result);
+int make_plot();
 
 void initialize_buttons(s21_numpad *numpad);
 void initialize_upper_grid(GtkWidget *upper_grid, s21_limits *limits);
